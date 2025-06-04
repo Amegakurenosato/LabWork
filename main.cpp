c132997d90b7409064af2e0c933adf44eeeca510
Amegakurenosato <klimenko.semen27777@gmail.com>
2025-06-05 02:18:52 +0300

    Объединение изменений: добавлены произведение и частное

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 // Учебная программа для работы с Git
-#include "sdt.h"
+#include <iostream>
+using namespace std;
 
 int main() {
     int a, b;
     cout << "Введите два целых числа: ";
     cin >> a >> b;
     cout << "Сумма чисел: " << a + b << endl;
+    cout << "Разность чисел: " << a - b << endl;
     cout << "Произведение чисел: " << a * b << endl;
     cout << "Частное чисел: " << a / b << endl;
     return 0;
